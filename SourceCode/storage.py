dfc578a5085ba3dcd29f4eb87e438a0137c56979
Doorbell9 <harrison.dorber@students.plymouth.ac.uk>
2026-01-06 20:33:47 +0000

    added database initialisation in SQLite

---
+++ SourceCode/storage.py
@@ -0,0 +1,58 @@
+import sqlite3
+import os
+
+db_name = "database.db"
+
+
+def initialise_database():
+    conn = sqlite3.connect(db_name)
+    conn.execute("PRAGMA foreign_keys = ON")
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Users (
+            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL UNIQUE,
+            role TEXT NOT NULL DEFAULT 'User' CHECK (role IN ('User', 'Admin')),
+            subscription_date DATE,
+            rank TEXT NOT NULL DEFAULT 'rookie'
+        )
+    ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Agents (
+            agent_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL UNIQUE,
+            description TEXT
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Locations (
+            location_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL UNIQUE,
+            description TEXT
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Departments (
+            department_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL UNIQUE,
+            description TEXT
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Archives (
+            archive_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            content TEXT,
+            agent_id INTEGER,
+            location_id INTEGER,
+            FOREIGN KEY(agent_id) REFERENCES Agents(agent_id),
+            FOREIGN KEY(location_id) REFERENCES Locations(location_id)
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
